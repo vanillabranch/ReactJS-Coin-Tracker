22e4a404a1b7bbccaa25135a85252915a7038097
vanillabranch <lockdown@kakao.com>
2021-11-08 22:18:59 +0900

    api url을 가져와서 첫 로딩화면 후, 데이터 수신이 완료되면 로딩화면 없애고&api내역 보이게 하는 방식으로 처리하는 미니 프로젝트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div >
-
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+    const [coins,setCoins] = useState([]);
+    useEffect(()=>{
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json)=>{
+                setCoins(json);
+                setLoading(false);
+            });
+    },[]); //감지하라고 아무것도 지정하지 않았기때문에 무조건 1회만 실행된다는거 잊지말자
+    return (
+        <div>
+            {/*삼항 연산자를 이용하여 컴포넌트나 값을 보여주고, 안보여주고..잘 기억하자 */}
+            <h1>The Coins!  {loading ? "" : `(${coins.length})`} </h1>
+            {loading ? <strong>Loading...</strong> :
+                <select>{/*map에서 함수선언하는거, 중괄호 말고 소괄호다. 그리고 key값을 id다. 반드시 입력할것!*/}
+                    {coins.map((coin)=>(
+                        <option key={coin.id}>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</option>
+                    ))}
+                </select>
+            }
+        </div>
+    );
 }
 
 export default App;
